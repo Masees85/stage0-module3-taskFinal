0fba89addd90326eb5ebf86343046e0954eb8412
Masees85 <109299067+Masees85@users.noreply.github.com>
2022-08-08 09:36:29 -0400

    Update NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
+        
+        System.out.print(number>0);
+        
+        
 
     }
 }
