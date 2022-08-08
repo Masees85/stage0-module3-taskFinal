ff6119483f87e6406da3f8aeab703b0dec922ad5
Masees85 <109299067+Masees85@users.noreply.github.com>
2022-08-08 09:31:25 -0400

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,21 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        
+  int  digit, sum = 0; 
+  digit = number % 10;  
+  sum = sum + digit;  
+  number = number / 10;  
+  digit = number % 10;  
+  sum = sum + digit;  
+  number = number / 10;  
+  digit = number % 10;  
+  sum = sum + digit;  
+  number = number / 10;  
+  digit = number % 10;  
+  sum = sum + digit;  
+       
+  System.out.println(sum);
+        
     }
 }
